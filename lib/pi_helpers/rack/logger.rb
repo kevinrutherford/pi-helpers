fc2843aa16deed29857503f9a06f48abfbf49ef5
Kevin Rutherford <kevin@rutherford-software.com>
2019-02-12 12:26:59 +0000

    Fix stupid typo

--- lib/pi_helpers/rack/logger.rb
+++ lib/pi_helpers/rack/logger.rb
@@ -19,34 +19,32 @@ module Pi
       def call(env)
         env['pi.logger'] = @writer
         req = Rack::Request.new(env)
-        begin
-          status, headers, body = @app.call(env)
-          @writer.call({
-            level:  'info',
-            tag:    'http.request',
-            msg:    "#{env['REQUEST_METHOD']} #{req.fullpath}",
-            status: status
-          })
-          [status, headers, body]
-        rescue Exception => ex
-          @listener.call({
-            level: 'error',
-            tag: 'internal.error',
-            msg: ex.message,
-            classname: ex.class.name,
-            stacktrace: ex.backtrace,
-            request: "#{env['REQUEST_METHOD']} #{req.fullpath}",
-          })
-          Pi::Rack.respond(500, {
-            errors: [
-              {
-                status: '500',
-                title: 'Internal server error',
-                detail: ex.message
-              }
-            ]
-          })
-        end
+        status, headers, body = @app.call(env)
+        @writer.call({
+          level:  'info',
+          tag:    'http.request',
+          msg:    "#{env['REQUEST_METHOD']} #{req.fullpath}",
+          status: status
+        })
+        [status, headers, body]
+      rescue Exception => ex
+        @writer.call({
+          level: 'error',
+          tag: 'internal.error',
+          msg: ex.message,
+          classname: ex.class.name,
+          stacktrace: ex.backtrace,
+          request: "#{env['REQUEST_METHOD']} #{req.fullpath}",
+        })
+        Pi::Rack.respond(500, {
+          errors: [
+            {
+              status: '500',
+              title: 'Internal server error',
+              detail: ex.message
+            }
+          ]
+        })
       end
 
     end
